7e36a16ff2010d2de710c8bd467f4136f3058907
Dip908 <dipchondo007@gmail.com>
2026-01-26 17:53:04 +0600

    create user register controller and add asynce handler function

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,16 +1,39 @@
 import User from "../model/User.js";
+import { ApiError } from "../ulits/ApiError.js";
+import { ApiResponse } from "../ulits/ApiResponse.js";
+import { asynceHandler } from "../ulits/asyncHandler.js";
 
-export const handleRegisterUser = async (req,res) => {
-    // get user details
-
-    // username
-    // email
-    // password
-    // check user already in database or not
-    // if yes return already exit 
-    // else create new user
+export const handleRegisterUser = asynceHandler(async (req, res) => {
 
     const { username, email, password } = req.body
-    console.log(username, email, password)
-    res.status(200).json("4444")
-}
+
+    if (!username) {
+        throw new ApiError(400, "username is required")
+    }
+
+    if (!email) {
+        throw new ApiError(400, "email is required")
+    }
+
+    if (!password) {
+        throw new ApiError(400, "password is required")
+    }
+
+    const isUserExist = await User.findOne({ email })
+    if (isUserExist) {
+        throw new ApiError(409, "This already used. Please Try with a new account Or Login")
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password
+    })
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            user,
+            "Registration Sucessfull"
+        ))
+})
--- model/User.js
+++ model/User.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (){
     if(this.isModified("password")){
-        this.password=bcrypt.hash(this.password,10)
+        this.password= await bcrypt.hash(this.password,10)
     }
 })
 
