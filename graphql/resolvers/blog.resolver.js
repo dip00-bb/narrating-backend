7062ef38cafd12d2b1847c056fdb527bec54e18e
dip00-bb <dipparthochondo@gmail.com>
2026-02-09 14:27:10 +0600

    create a blog resolver and a index.js file to combile all resolvers

---
+++ graphql/resolvers/blog.resolver.js
@@ -0,0 +1,16 @@
+const Blog=[
+    {
+        _id:"123",
+        title:"Hello world",
+        description:"I am hello world blog"
+    }
+
+]
+
+export const blogResolver={
+    Query:{
+        blogs: async ()=>{
+            return 
+        }
+    }
+}
