7617a8460afef4b4c968a56af5fe3cd04108bbda
dip00-bb <dipparthochondo@gmail.com>
2026-02-23 17:38:46 +0600

    add the liking feature

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -2,17 +2,17 @@ import Blog from "../model/Blog.js";
 import { ApiResponse } from "../ulits/ApiResponse.js"
 import { asynceHandler } from "../ulits/asyncHandler.js"
 
-export const handleCreateNewBlog = asynceHandler(async(req, res)=>{
-    const {content,coverImage,title}=req.body;
+export const handleCreateNewBlog = asynceHandler(async (req, res) => {
+    const { content, coverImage, title } = req.body;
 
     await Blog.create({
-        creatorId:req.user._id,
+        creatorId: req.user._id,
         content,
         coverImage,
         title,
     })
 
-        res
+    res
         .status(200)
         .json(
             new ApiResponse(
@@ -21,4 +21,30 @@ export const handleCreateNewBlog = asynceHandler(async(req, res)=>{
                 "Blog Created Successfully"
             )
         )
-} )
+})
+
+
+export const handleLikeBlog = asynceHandler(async (req, res) => {
+    const { id } = req.params
+
+    console.log(id)
+
+    await Blog.updateOne(
+        { _id: id },
+        {
+            $inc: {
+                totalLikes: 1
+            }
+        }
+    )
+
+    res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Like Added"
+            )
+        )
+})
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { handleCreateNewBlog } from "../controller/blog.controller.js";
+import { handleCreateNewBlog, handleLikeBlog } from "../controller/blog.controller.js";
 import { verifyUser } from "../middlewares/auth.middleware.js";
 
 const router=Router()
 
 router.post('/create-new-blog',verifyUser(), handleCreateNewBlog)
-
+router.post('/like-blog/:id', handleLikeBlog)
 export default router
