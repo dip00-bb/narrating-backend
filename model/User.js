7cb84ae0bb8a788d611adbaf9864565cdde385c3
dip00-bb <dipparthochondo@gmail.com>
2026-02-23 17:03:31 +0600

    add total subscriber and total follower in user schema

--- model/User.js
+++ model/User.js
@@ -16,10 +16,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    role: {
-        type: String,
-    },
-
     profileImage: {
         type: String,
 
@@ -32,12 +28,20 @@ const userSchema = new mongoose.Schema({
     },
     resetPassToken: {
         type: String
+    },
+    totalFollower: {
+        type: Number,
+        default: 0
+    },
+    totalSubscriber: {
+        type: Number,
+        default: 0
     }
 })
 
 userSchema.pre("save", async function () {
     if (this.isModified("password")) {
-       return this.password = await bcrypt.hash(this.password, 10)
+        return this.password = await bcrypt.hash(this.password, 10)
     }
 })
 
