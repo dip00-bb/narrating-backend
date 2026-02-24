b92fa4673589576d8c648104608ccb82bcb82fde
dip00-bb <dipparthochondo@gmail.com>
2026-02-24 19:55:11 +0600

    in zod validation middleware add body and params key so that access them from request object

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -2,13 +2,16 @@ import * as z from 'zod'
 
 export const validator = (schema) => {
     return (req, res, next) => {
-        const result = schema.safeParse(req.body);
+        const result = schema.safeParse({
+            body: req.body,
+            params: req.params
+        });
 
         if (result.success) {
             next()
         } else if (result.error instanceof z.ZodError) {
             console.log(result.error.issues[0].message)
-            res.json({ message: result.error.issues })
+            return res.json({ message: result.error.issues })
         }
     }
 }
