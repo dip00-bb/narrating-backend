a306b3444c3c30c468670b8dd2f7067a58069ad0
dip00-bb <dipparthochondo@gmail.com>
2026-02-25 17:33:13 +0600

    implement update comment feature

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -6,10 +6,10 @@ import { asynceHandler } from "../ulits/asyncHandler.js";
 
 export const handleAddComment = asynceHandler(async (req, res) => {
     const { id } = req.params
-    const {content}=req.body
+    const { content } = req.body
 
-    if(!content){
-        throw new ApiError(400,"Content Field Can't be Empty")
+    if (!content) {
+        throw new ApiError(400, "Content Field Can't be Empty")
     }
 
     const commentorId = req.user.id
@@ -18,11 +18,11 @@ export const handleAddComment = asynceHandler(async (req, res) => {
 
 
     await Comment.create({
-        author:commentor.username,
-        authorId:commentor._id,
-        blogId:id,
-        comment:content,
-        replideTo:null
+        author: commentor.username,
+        authorId: commentor._id,
+        blogId: id,
+        comment: content,
+        replideTo: null
 
     })
 
@@ -39,3 +39,28 @@ export const handleAddComment = asynceHandler(async (req, res) => {
 })
 
 
+export const handleUpdateComment = asynceHandler(async (req, res) => {
+    const { id } = req.params
+    const { content } = req.body
+
+    if (!content) {
+        throw new ApiError(400, "Content Field Can't be Empty")
+    }
+
+    const updatedDocument = await Comment.findByIdAndUpdate(id, { comment: content },{new:true})
+
+    if(!updatedDocument){
+        throw new ApiError(404, "No Comment Found")
+    }
+
+    res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Comment updated Successfully"
+            )
+        )
+
+})
--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { handleAddComment } from "../controller/comment.controller.js";
+import { handleAddComment, handleUpdateComment } from "../controller/comment.controller.js";
 import { verifyUser } from "../middlewares/auth.middleware.js";
 import { validator } from "../middlewares/validationMiddleware.js";
 import { commentSchema } from "../schemas/commentSchema.js";
 
 const router=Router()
 
-router.post('/create-comment/:id',verifyUser(), validator(commentSchema), handleAddComment)
-
+router.post('/create-comment/:id',verifyUser(), validator(commentSchema),handleAddComment)
+router.patch('/update-comment/:id',verifyUser(), validator(commentSchema),handleUpdateComment)
 export default router
