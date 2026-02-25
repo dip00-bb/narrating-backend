e4273bbbb72e51dc68830c59191fc3265aacef00
dip00-bb <dipparthochondo@gmail.com>
2026-02-25 16:19:29 +0600

    in add comment section return 400 status code for invalid input

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -1,11 +1,16 @@
 import Comment from "../model/Comment.js";
 import User from "../model/User.js";
+import { ApiError } from "../ulits/ApiError.js";
 import { ApiResponse } from "../ulits/ApiResponse.js";
 import { asynceHandler } from "../ulits/asyncHandler.js";
 
 export const handleAddComment = asynceHandler(async (req, res) => {
     const { id } = req.params
     const {content}=req.body
+
+    if(!content){
+        throw new ApiError(400,"Content Field Can't be Empty")
+    }
 
     const commentorId = req.user.id
 
